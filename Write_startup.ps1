745c0b9190974e0c10206b0547747ed8dd789ab6
mickdec <mickaeldecelle@hotmail.fr>
2022-05-19 11:44:36 +0200

    OKOK

--- Write_startup.ps1
+++ Write_startup.ps1
@@ -1,3 +1,2 @@
 Set-Content -Path $env:APPDATA"/Microsoft/Windows/Start Menu/Programs/Startup/rsyslog_91273.bat" -Value 'Nothing to see here.'
-del  $env:APPDATA"/Microsoft/Windows/Start Menu/Programs/Startup/rsyslog_91273.bat"
-reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Persist_SOC" /r REG_SZ /d "C:\1.exe" /f
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Persist_SOC" /t REG_SZ /d "C:\1.exe" /f
